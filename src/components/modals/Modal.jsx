3d88fe690e905352dc88d7eed02858f70998fdd1
kaamosdao <kaamosdao@gmail.com>
2023-04-13 17:45:32 +0300

    Change Modal: add Form to ModalForm, change border

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames/bind';
 
+import { closeModal } from '../../slices/modalSlice';
 import selectShow, { selectModal } from '../../slices/modalSelectors';
 
 import ModalForm from './ModalForm';
 
 import s from './Modal.module.scss';
-import { closeModal } from '../../slices/modalSlice';
 
 const cn = classNames.bind(s);
 
--- src/components/modals/ModalForm.jsx
+++ src/components/modals/ModalForm.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { closeModal } from '../../slices/modalSlice';
+
+import AddContactForm from './AddContactForm';
 
 import s from './ModalForm.module.scss';
 
 const ModalForm = () => {
+  const dispatch = useDispatch();
+
   const handleClick = (e) => {
     e.stopPropagation();
   };
+
+  const handleClose = () => {
+    dispatch(closeModal());
+  };
+
   return (
     <div className={s.modalForm} onClick={handleClick} aria-hidden="true">
-      Form
+      <button className={s.buttonClose} type="button" onClick={handleClose}>
+        <span className="visually-hidden">Close</span>+
+      </button>
+      <AddContactForm />
     </div>
   );
 };
