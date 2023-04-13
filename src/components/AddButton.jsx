3a9e83fa11389b8e2cd89dbca71255a38feb79e4
kaamosdao <kaamosdao@gmail.com>
2023-04-13 15:41:47 +0300

    Refactor AddButton; fix prettier warns

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -3,22 +3,22 @@ import { useDispatch } from 'react-redux';
 
 import { showModal } from '../slices/modalSlice';
 
-import modalType from '../types/types'
+import modalType from '../types/types';
 
 import s from './AddButton.module.scss';
 
 const AddButton = () => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-const handleClick = () => {
-  dispatch(showModal({ type: modalType.form }));
-}
+  const handleClick = () => {
+    dispatch(showModal({ type: modalType.form }));
+  };
 
-return (
-  <button className={s.button} type="button" onClick={handleClick}>
-    Add contact
-  </button>
-)
-} ;
+  return (
+    <button className={s.button} type="button" onClick={handleClick}>
+      Add contact
+    </button>
+  );
+};
 
 export default AddButton;
