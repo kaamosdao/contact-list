612b7eda1b921331314b06b6da300edb16975c50
kaamosdao <kaamosdao@gmail.com>
2023-04-13 14:51:12 +0300

    Add Main: add main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import AddButton from './AddButton';
+
+import s from './Main.module.scss';
+
+const Main = () => (
+    <main className={s.main}>
+      <h1 className={s.title}>Contacts</h1>
+      <section className={s.addButton}>
+        <h2 className="visually-hidden">Add Button</h2>
+        <AddButton />
+      </section>
+    </main>
+  );
+
+export default Main;
