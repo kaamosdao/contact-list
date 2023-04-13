74062d09327933510af2f467f52c693ba6864e94
kaamosdao <kaamosdao@gmail.com>
2023-04-13 14:51:40 +0300

    Add AddButton: create AddButton components

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './AddButton.module.scss';
+
+const AddButton = () => (
+  <button className={s.button} type='button'>Add contact</button>
+);
+
+export default AddButton;
