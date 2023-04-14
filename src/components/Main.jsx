a6fc0bc2d7f0ae8676257589a1cd7aa4357e5b43
kaamosdao <kaamosdao@gmail.com>
2023-04-14 16:36:11 +0300

    Change Main: add contacts section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import AddButton from './AddButton';
+import ContactList from './ContactList';
 
 import s from './Main.module.scss';
 
 const Main = () => (
   <main className={s.main}>
     <h1 className={s.title}>Contacts</h1>
-    <section className={s.addButton}>
+    <section className={s.addSection}>
       <h2 className="visually-hidden">Add Button</h2>
       <AddButton />
+    </section>
+    <section className={s.contacts}>
+      <ContactList />
     </section>
   </main>
 );
