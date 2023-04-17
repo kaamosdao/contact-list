87e6079fc74ca5b318c0ebba88aedbc28c88d7fb
kaamosdao <kaamosdao@gmail.com>
2023-04-17 13:27:19 +0300

    Refactor store: remove modal slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import modalReducer from './slices/modalSlice.js';
 import contactReducer from './slices/contactSlice.js';
 
 import localStorageMiddleware from './middlewares/localStorageMiddleware.js';
 
 export default configureStore({
   reducer: {
-    modals: modalReducer,
     contacts: contactReducer,
   },
   middleware: (getDefaultMiddleware) =>
