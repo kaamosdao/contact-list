4b33629104dbd56c53af79459e591fe64f29206b
kaamosdao <kaamosdao@gmail.com>
2023-04-17 15:36:50 +0300

    Change Modal: add modal for contacts

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -3,35 +3,45 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
 import ModalForm from './ModalForm';
+import ModalContact from './ModalContact';
 
 import s from './Modal.module.scss';
 
 const cn = classNames.bind(s);
 
-const Modal = ({ showModal, setShowModal, type }) => {
+const modals = {
+  form: (setShowModal) => <ModalForm setShowModal={setShowModal} />,
+  contact: (setShowModal, modalData) => (
+    <ModalContact setShowModal={setShowModal} contact={modalData} />
+  ),
+};
+
+const Modal = ({ setShowModal, type, modalData }) => {
   const handleClick = () => {
     setShowModal(false);
   };
 
-  const modals = {
-    form: <ModalForm setShowModal={setShowModal} />,
-  };
-
   return (
-    <div
-      className={cn('modal', { show: showModal })}
-      onClick={handleClick}
-      aria-hidden="true"
-    >
-      {modals[type]}
+    <div className={cn('modal')} onClick={handleClick} aria-hidden="true">
+      {modals[type](setShowModal, modalData)}
     </div>
   );
 };
 
 Modal.propTypes = {
-  showModal: PropTypes.bool.isRequired,
   setShowModal: PropTypes.func.isRequired,
   type: PropTypes.string.isRequired,
+  modalData: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+  }),
+};
+
+Modal.defaultProps = {
+  modalData: null,
 };
 
 export default Modal;
---
+++ src/components/modals/ModalContact.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import s from './ModalContact.module.scss';
+
+const ModalContact = ({ contact, setShowModal }) => {
+  const handleClick = (e) => {
+    e.stopPropagation();
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className={s.modalContact} onClick={handleClick} aria-hidden="true">
+      <button className={s.buttonClose} type="button" onClick={handleClose}>
+        <span className="visually-hidden">Close</span>+
+      </button>
+      <div className={s.contactCard}>
+        <div className={s.name}>
+          <span>{contact.name}</span>
+          &nbsp;
+          <span>{contact.surname}</span>
+        </div>
+        <ul className={s.contacts}>
+          <li className={s.email}>
+            <span className={s.tag}>email:</span>&nbsp;{contact.email}
+          </li>
+          <li>
+            <span className={s.tag}>phone:</span>&nbsp;{contact.phone}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+ModalContact.propTypes = {
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+  }).isRequired,
+  setShowModal: PropTypes.func.isRequired,
+};
+
+export default ModalContact;
