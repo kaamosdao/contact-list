bcc26c7e04aa73a5b8657a47967d35ce8d93c3c4
kaamosdao <kaamosdao@gmail.com>
2023-04-25 16:33:49 +0300

    Change ModalContact: render birthday, relations

--- src/components/modals/ModalContact.jsx
+++ src/components/modals/ModalContact.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import s from './styles/ModalContact.module.scss';
 
 const ModalContact = ({ contact, setShowModal }) => {
+  const birthDay = new Date(contact.birthday).toLocaleDateString('ru');
   const handleClick = (e) => {
     e.stopPropagation();
   };
@@ -14,6 +15,7 @@ const ModalContact = ({ contact, setShowModal }) => {
 
   return (
     <div className={s.modalContact} onClick={handleClick} aria-hidden="true">
+      <h2 className="visually-hidden">Contact Card</h2>
       <button className={s.buttonClose} type="button" onClick={handleClose}>
         <span className="visually-hidden">Close</span>+
       </button>
@@ -23,6 +25,18 @@ const ModalContact = ({ contact, setShowModal }) => {
           &nbsp;
           <span>{contact.surname}</span>
         </div>
+        <ul className={s.relations}>
+          {contact.relations.map(({ value, id }) => (
+            <li className={s.relationsItem} key={id}>
+              {value}
+            </li>
+          ))}
+        </ul>
+        <div className={s.birthday}>
+          <span className={s.birthTag}>birthday:</span>&nbsp;{birthDay}
+        </div>
+        <hr className={s.separator} />
+        <h3 className={s.contactTitle}>Contacts:</h3>
         <ul className={s.contacts}>
           <li className={s.email}>
             <span className={s.tag}>email:</span>&nbsp;{contact.email}
@@ -43,6 +57,14 @@ ModalContact.propTypes = {
     surname: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
     phone: PropTypes.string.isRequired,
+    birthday: PropTypes.string.isRequired,
+    relations: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        value: PropTypes.string,
+        label: PropTypes.string,
+      })
+    ).isRequired,
   }).isRequired,
   setShowModal: PropTypes.func.isRequired,
 };
