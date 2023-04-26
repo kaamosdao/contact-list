f0e99ca9b79f26b708acbd5d12017aba795bd249
kaamosdao <kaamosdao@gmail.com>
2023-04-26 17:29:41 +0300

    Change types: add types for button

--- src/types/types.js
+++ src/types/types.js
@@ -3,4 +3,15 @@ const modalType = {
   contact: 'contact',
 };
 
+export const buttonStyle = {
+  submit: 'buttonSubmit',
+  add: 'buttonAdd',
+};
+
+export const buttonType = {
+  submit: 'submit',
+  button: 'button',
+  reset: 'reset',
+};
+
 export default modalType;
