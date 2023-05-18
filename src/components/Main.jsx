584cd1b39cb1c95b7eb3c8cb86e73976e35be1de
kaamosdao <kaamosdao@gmail.com>
2023-05-18 17:54:19 +0300

    Change Main: add section with filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SectionAddButton from './SectionAddButton';
+import SectionFilter from './SectionFitler';
 import SectionContactList from './SectionContactList';
 
 import s from './styles/Main.module.scss';
@@ -9,6 +10,7 @@ const Main = () => (
   <main className={s.main}>
     <h1 className={s.title}>Contacts</h1>
     <SectionAddButton />
+    <SectionFilter />
     <SectionContactList />
   </main>
 );
