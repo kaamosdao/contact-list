1a17b372be17573b152c3d18a133e4c43e5a8ecf
kaamosdao <kaamosdao@gmail.com>
2023-05-19 17:24:50 +0300

    Change App: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import React from 'react';
-import Main from './components/Main';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const App = () => <Main />;
+import Main from './components/Main';
+import Notfound from './components/Notfound';
+import ContactPage from './components/ContactPage';
+
+import store from './store/index.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <Notfound />,
+  },
+  {
+    path: 'contacts/:contactIndex',
+    loader: ({ params }) => {
+      const contact = store.getState().contacts.items[params.contactIndex - 1];
+      return contact;
+    },
+    element: <ContactPage />,
+    errorElement: <Notfound />,
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
